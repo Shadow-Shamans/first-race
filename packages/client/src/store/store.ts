6b43934841e4ec48cc24d23a87a7e760fa2c3a1b
Vasily Guzov <vasily.guzov@gmail.com>
2023-03-23 02:52:40 +0300

    [infra][store] #1 fix serializable error redux-persist

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { reducer } from './rootReducer'
-import persistStore from 'redux-persist/es/persistStore'
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
 export const store = configureStore({
   reducer,
   devTools: import.meta.env.MODE !== 'producton',
-  middleware: getDefaultMiddleware => getDefaultMiddleware(),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // fix for serializableCheck error after base setup redux-persis
+        // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
