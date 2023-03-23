3aaf539e2542b2d1b835451fa21d05606c1e7cf1
Vasily Guzov <vasily.guzov@gmail.com>
2023-03-23 04:31:57 +0300

    [deps] #1 for supports css moudles in the tests

--- packages/client/src/features/User/userSlice.ts
+++ packages/client/src/features/User/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { ThemeNames } from '../../App'
+import { ThemeNames } from '../../app'
 
 import { RootState } from '../../store'
 
