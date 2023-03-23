9b41c42b4d6f875d314569cf3233b0ba7631a28f
Vasily Guzov <vasily.guzov@gmail.com>
2023-03-23 04:32:33 +0300

    [infra] #1 additional config for support css modules

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,10 +1,14 @@
 import dotenv from 'dotenv'
+import 'whatwg-fetch'
 dotenv.config()
 
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
