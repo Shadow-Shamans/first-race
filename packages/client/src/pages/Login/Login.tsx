7eb4bc101ffc37dc6c33b838b09572e49a162400
borisovaas <borisovaas@oooinex.ru>
2023-03-31 21:27:17 +0300

    Валидация форм

--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -24,7 +24,7 @@ export const Login: FC = () => {
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
+          style={{ width: 420 }}
           initialValues={{ remember: true }}
           onFinish={onSuccessSubmit}
           onFinishFailed={onErrorSubmit}
--- packages/client/src/pages/Registration/Registration.tsx
+++ packages/client/src/pages/Registration/Registration.tsx
@@ -1,5 +1,78 @@
 import { FC } from 'react'
+import { Button, Form, Input } from 'antd'
+import { LayoutFullPage } from '@/components/LayoutFullPage'
+import { LinkToPage } from '@/components/LinkToPage'
+import validateForms from '@/shared/utils/validateForms'
 
 export const Registration: FC = () => {
-  return <div>Registration page</div>
+  const [form] = Form.useForm()
+
+  const onCheck = async () => {
+    try {
+      const values = await form.validateFields()
+
+      if (!values.errorFields) {
+        console.log(values)
+      }
+    } catch (errorInfo) {
+      console.log(errorInfo)
+    }
+  }
+
+  return (
+    <LayoutFullPage>
+      <div>
+        <Form
+          form={form}
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ width: 420 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          size="large">
+          <Form.Item
+            name="name"
+            label="Имя"
+            rules={[
+              {
+                required: true,
+                message: 'Поле имени не может быть пустым!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  return validateForms(getFieldValue, 'name', value)
+                },
+              }),
+            ]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="Пароль"
+            rules={[
+              {
+                required: true,
+                message: 'Поле пароля не может быть пустым!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  return validateForms(getFieldValue, 'password', value)
+                },
+              }),
+            ]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" onClick={onCheck}>
+              Регистрация
+            </Button>
+          </Form.Item>
+        </Form>
+        <LinkToPage text="Нет аккаунта?" to="/registration" />
+      </div>
+    </LayoutFullPage>
+  )
 }
