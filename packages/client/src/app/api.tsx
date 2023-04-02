395c1dc9052202124326d560f3784a10a7dbcf22
Vasily Guzov <vasily.guzov@gmail.com>
2023-04-02 23:21:32 +0300

    [fix] local dev run project

--- packages/client/src/app/api.tsx
+++ packages/client/src/app/api.tsx
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { TTestData } from './types'
 
-const PORT = process.env.__SERVER_PORT__ || 3001
-
 export const testDataApi = createApi({
   reducerPath: 'testDataApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:${PORT}` }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `http://localhost:${__SERVER_PORT__}`,
+  }),
   endpoints: builder => ({
     getTestData: builder.query<TTestData, string>({
       query: () => `/`,
