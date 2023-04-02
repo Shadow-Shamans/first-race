f6fe297c9d529189671f248d36cab4bbf66c758c
Vasily Guzov <vasily.guzov@gmail.com>
2023-04-02 22:16:18 +0300

    [fix][jest] error port

--- packages/client/src/app/api.tsx
+++ packages/client/src/app/api.tsx
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { TTestData } from './types'
 
+const PORT = process.env.__SERVER_PORT__ || 3001
+
 export const testDataApi = createApi({
   reducerPath: 'testDataApi',
-  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:${__SERVER_PORT__}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:${PORT}` }),
   endpoints: builder => ({
     getTestData: builder.query<TTestData, string>({
       query: () => `/`,
