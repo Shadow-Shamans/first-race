276af21217a059129b51200ee125dc3ecd605373
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-04-08 09:41:53 +0300

    fix: fixed styles in forum

--- packages/client/src/features/ForumTopicItem/ForumTopicItem.tsx
+++ packages/client/src/features/ForumTopicItem/ForumTopicItem.tsx
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom'
 import { IProps } from './types'
 import styles from './ForumTopicItem.module.css'
 
+// TODO разобраться с цветами badge
 export const ForumTopicItem: FC<IProps> = ({ item, isLoading }) => (
   <List.Item className={styles.listItem}>
     <Skeleton avatar title={false} loading={isLoading} active>
       <List.Item.Meta
-        avatar={<Avatar shape="circle" size="default" src={item.userAvatar} />}
         title={
           <Link to={`/forum/topic/${item.id}`} className={styles.title}>
             {item.title}
           </Link>
         }
         description={
-          <Typography className={styles.lastMessage}>
-            {item.lastMessage}
-          </Typography>
+          <div className={styles.description}>
+            <Avatar shape="circle" size="default" src={item.userAvatar} />
+            <Typography className={styles.lastMessage}>
+              {item.lastMessage}
+            </Typography>
+          </div>
         }
       />
       <Badge
