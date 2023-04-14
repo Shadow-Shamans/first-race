0014d96da8c33418d26a131b5ece28a0f57b371c
Daria Lobanova <dlobanova@sberhealth.ru>
2023-04-14 21:20:13 +0300

    apply service workers

--- packages/client/src/shared/utils/sw.ts
+++ packages/client/src/shared/utils/sw.ts
@@ -1,3 +1,5 @@
+export const cacheName = 'first-race-cache-v1'
+
 export const initServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
@@ -16,4 +18,22 @@ export const initServiceWorker = () => {
   } else {
     console.error('Service worker in not supported in your browser')
   }
+}
+
+export const getFromNetwork = (event: FetchEvent) => {
+  const fetchRequest = event.request.clone()
+
+  return fetch(fetchRequest).then(response => {
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      return response
+    }
+
+    const responseToCache = response.clone()
+
+    caches.open(cacheName).then(cache => {
+      cache.put(event.request, responseToCache)
+    })
+
+    return response
+  })
 }
--- packages/client/src/sw.ts
+++ packages/client/src/sw.ts
@@ -1,5 +1,33 @@
 import { precacheAndRoute } from 'workbox-precaching'
+import { cacheName, getFromNetwork } from '@/shared/utils/sw'
 
 declare const self: ServiceWorkerGlobalScope
 
 precacheAndRoute(self.__WB_MANIFEST)
+
+const urls = ['/', '/index.html', '/assets']
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => cache.addAll(urls))
+      .catch(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response
+      }
+
+      return getFromNetwork(event)
+    })
+  )
+})
