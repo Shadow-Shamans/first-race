e18f4f81c1f5d3a1bc8587e24d1aecf71c1b0e46
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-04-18 21:14:53 +0300

    fix: added classnames

--- packages/client/src/features/ForumMessages/ForumMessage.tsx
+++ packages/client/src/features/ForumMessages/ForumMessage.tsx
@@ -1,5 +1,6 @@
 import { IForumTopicMessage } from '@/pages/ForumTopic/types'
 import { Avatar, List, Skeleton, Typography } from 'antd'
+import classNames from 'classnames'
 import { FC } from 'react'
 import styles from './ForumMessages.module.css'
 
@@ -19,16 +20,19 @@ export const ForumMessage: FC<IMessageProps> = ({
   <List.Item>
     <Skeleton avatar title={false} loading={isLoading} active>
       <List.Item.Meta
-        className={`${
-          idx % 2 === 0 ? styles.listItem : styles.listItemReverse
-        }`}
+        className={classNames({
+          [styles.listItem]: idx % 2 === 0,
+          [styles.listItemReverse]: idx % 2 !== 0,
+        })}
         key={message.userId}
         avatar={
           <Avatar
             shape="circle"
             size="default"
             src={message.avatar}
-            className={`${idx % 2 !== 0 ? styles.avatarReverse : ''}`}
+            className={classNames({
+              [styles.avatarReverse]: idx % 2 !== 0,
+            })}
           />
         }
         description={
