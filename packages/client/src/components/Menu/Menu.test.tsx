529bfb5c3bc04780f3189b840a6b697f1f05974e
Daria Lobanova <dlobanova@sberhealth.ru>
2023-04-20 11:03:12 +0300

    fix tests

--- packages/client/src/components/Menu/Menu.test.tsx
+++ packages/client/src/components/Menu/Menu.test.tsx
@@ -10,28 +10,35 @@ describe('Menu test cases', () => {
         <Menu />
       </MemoryRouter>
     )
+
     expect(screen.getByText('Главная')).toBeInTheDocument()
     expect(screen.getByText('Логин')).toBeInTheDocument()
     expect(screen.getByText('Регистрация')).toBeInTheDocument()
     expect(screen.getByText('Профиль')).toBeInTheDocument()
     expect(screen.getByText('Форум')).toBeInTheDocument()
     expect(screen.getByText('Играть')).toBeInTheDocument()
+    expect(screen.getByText('Лидерборд')).toBeInTheDocument()
   })
+
   it('Should render all menu items', () => {
     render(
       <MemoryRouter>
         <Menu />
       </MemoryRouter>
     )
-    expect(screen.getAllByTestId('menu-item')).toHaveLength(5)
+
+    expect(screen.getAllByTestId('menu-item')).toHaveLength(6)
   })
+
   it('Should render new route', () => {
     render(
       <MemoryRouter>
         <Menu />
       </MemoryRouter>
     )
+
     const history = createMemoryHistory()
+
     history.push('/login')
     expect(history.location.pathname).toBe('/login')
   })
