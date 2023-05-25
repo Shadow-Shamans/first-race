ada6bebdb9271dbd0f5a79cfefb687f881ef433e
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-25 09:45:42 +0300

    [infra][refac] for readable setup different environment #102 #97

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,27 +1,29 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
-// import { createServer as createViteServer } from 'vite'
-// import type { ViteDevServer } from 'vite'
 import express from 'express'
 import { apiRouter, forumRouter } from './routes/api'
-// import * as fs from 'fs'
-// import * as path from 'path'
+import { setupDevServer, setupProdServer } from './envSetup'
 
 dotenv.config({ path: '../../.env' })
 
-// const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
 const port = Number(process.env.SERVER_PORT) || 3001
 
 const app = express()
-app.use(cors())
 
-app.get('/', (req, res) => {
-  console.log({ req, res })
-  res.send('<h2>Server</h2>')
-})
+app.use(cors())
 
 app.use('/api', apiRouter)
 app.use('/api/forum', forumRouter)
+
+if (isDev) {
+  setupDevServer(app)
+}
+
+if (isProd) {
+  setupProdServer(app)
+}
 
 app.listen(port, () => {
   console.log(`API listen on port ${port}`)
