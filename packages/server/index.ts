56432fb2ab5104529a4c404a39530e950afeff80
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-25 13:05:37 +0300

    [infra] connect database
    Для запуска приложения с базой данных нужно сначала запустить docker
    image c postgres.
    
    docker compose -f docker-compose.dev.yml up
    после этого запускаем сам сервер в режиме разработки.
    yarn dev:server
    
    todo: нужно оптимизировать запуск dev:server там почему то несколько раз
    он у нас клиент собирает.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,12 +3,15 @@ import cors from 'cors'
 import express from 'express'
 import { apiRouter, forumRouter } from './routes/api'
 import { setupDevServer, setupProdServer } from './envSetup'
+import { createClientAndConnect } from './db'
 
 dotenv.config({ path: '../../.env' })
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = process.env.NODE_ENV === 'production'
 const port = Number(process.env.SERVER_PORT) || 3001
+
+createClientAndConnect()
 
 const app = express()
 
