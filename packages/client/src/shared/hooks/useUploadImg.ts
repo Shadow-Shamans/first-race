4c94d92b311c992bf6b35b538fd752e0fada2d2f
Vasily Guzov <vasily.guzov@gmail.com>
2023-05-27 15:06:53 +0300

    [hook] for upload, validate, and convert images #91

---
+++ packages/client/src/shared/hooks/useUploadImg.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { validateImage } from '@/shared/utils'
+import type { UploadChangeParam } from 'antd/es/upload'
+import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface'
+import { getBase64 } from '@/shared/utils'
+
+export function useUploadImg(imgSrc: string) {
+  const [loading, setLoading] = useState(false)
+  const [imageUrl, setImageUrl] = useState<string>(imgSrc)
+
+  const handleChange: UploadProps['onChange'] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true)
+      return
+    }
+    if (info.file.status === 'done') {
+      getBase64(info.file.originFileObj as RcFile, url => {
+        setLoading(false)
+        setImageUrl(url)
+      })
+    }
+  }
+
+  return { handleChange, imageUrl, validateImage, loading }
+}
